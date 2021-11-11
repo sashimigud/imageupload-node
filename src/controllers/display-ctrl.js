f128e60e766130b0f631e981420672df75224b6d
trym <trym_helle@hotmail.com>
2021-11-11 22:40:35 +0100

    trying to add getImages

---
+++ src/controllers/display-ctrl.js
@@ -0,0 +1,18 @@
+const displayImages = async (req, res) => {
+  try {
+    await upload(req, res);
+
+    if (req.file == undefined) {
+      return res.send(`You must select a file.`);
+    }
+
+    return res.send(`File has been uploaded.`);
+  } catch (error) {
+    console.log(error);
+    return res.send(`Error when trying upload image: ${error}`);
+  }
+};
+
+module.exports = {
+  displayImages: displayImages,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const homeController = require("../controllers/home-ctrl");
-const uploadController = require("../controllers/upload-ctrl");
+const homeController = require('../controllers/home-ctrl');
+const uploadController = require('../controllers/upload-ctrl');
+const displayController = require('../controllers/display-ctrl');
 
-let routes = app => {
-  router.get("/", homeController.getHome);
-  router.post("/upload", uploadController.uploadFile);
+let routes = (app) => {
+  router.get('/', homeController.getHome);
+  router.post('/upload', uploadController.uploadFile);
+  router.get('/images', displayController.displayImages);
 
-  return app.use("/", router);
+  return app.use('/', router);
 };
 
 module.exports = routes;
